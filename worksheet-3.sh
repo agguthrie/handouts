281675a654d94ecd155f11b5e9615916fce577ea
agguthrie <amanda.guthrie.805@gmail.com>
2020-07-21 17:32:54 +0000

    Adding worksheets in bulk to repo

---
+++ worksheet-3.sh
@@ -0,0 +1,11 @@
+# configure git
+
+git config --global user.name agguthrie
+git config --global user.email amanda.guthrie.805@gmail.com
+
+# Link your local repository to the origin repository on GitHub, by
+# copying the code shown on your GitHub repo under the heading:
+# "…or push an existing repository from the command line"
+
+git remote add origin https://github.com/agguthrie/handouts.git
+git push -u origin master
